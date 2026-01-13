d72c680c18a277de477a98946199619e0f587098
Nathan Daily <ndaily@netmediagroup.com>
2026-01-13 04:11:27 +0000

    Fix: Update bbb/__init__.py imports to match current exports

--- bbb/__init__.py
+++ bbb/__init__.py
@@ -1,20 +1,33 @@
 # BBB (Boredom Buster Bot) shared library
+# Servo imports
 from bbb.servo import (
     ServoController,
     SERVO_ENABLED,
     GPIO_PIN,
+    HAND_DOWN,
+    HAND_UP,
+    HAND_MIDDLE,
     MOUTH_CLOSED,
     MOUTH_OPEN,
     MOUTH_HALF,
     init_servo,
-    move_mouth,
-    mouth_talking_animation,
-    laugh_animation,
+    get_servo,
+    get_servo_lock,
+    get_servo_error,
+    move_hand,
+    hand_talking_animation,
+    joke_setup_animation,
+    punchline_animation,
+    hand_slap_animation,
 )
 
+# TTS imports
 from bbb.tts import (
     check_tts_available,
+    init_tts,
+    get_tts_command,
     speak_with_pico,
+    speak_with_espeak,
     speak_text_sync,
     speak_text_async,
 )
